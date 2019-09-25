7cfc1b29af39ee26a12841e81af7d413624176c0
BerkanKaya <99059255@mydavinci.nl>
2019-09-25 11:33:56 +0200

    eerste commit

---
+++ game.js
@@ -0,0 +1,85 @@
+alert("Je bent een huurmoordenaar, en je word 2 keuzes aangeboden.");
+var keuzes = prompt ("De eerste keuze is om de amerikaanse president te vermoorden voor 5 miljoen euro, de 2de keuze is om je beste vriend zijn moeder te vermoorden voor 1 miljoen euro, 1 of 2 ?");
+
+if(keuzes == "1"){
+	alert("Oke je hebt dus er voor gekozen om de amerikaanse president te vermoorden");
+	var plan = prompt("De bieder vraagt of je een sniper of bazooka wilt om Trump te vermoorden");
+	}
+
+	if(plan == "sniper"){
+	alert("Oke je hebt dus voor de sniper gekozen, en wilt hem in stilte vermoorden");
+	var plan1 = prompt("De bieder verteld je dat Trump een afspraak heeft bij de macdonalds om 2uur, en dat je hem daar kan vermoorden, wil je hem daar vermoorden?");
+	}
+
+	if(plan1 == "ja"){
+	alert("Oke je wilt hem dan dus vermoorden");
+	alert("3 uur later....");
+	alert("Je ziet trump bij de MacDonalds, en je staat klaar met de sniper in je hand");
+	alert("Je richt op Trump, maar er staan bewakers naast hem...");
+	alert("Er is een kans dat je Trump mist, en een bewaker raakt");
+	var plan2 = prompt("Schiet je meteen?");
+	}
+
+	if(plan2 == "ja"){
+	alert("JE SCHIET....");
+	alert("Je RAAKT Trump maar ook een kind met wie hij aan het praten was...");
+	var plan3 = prompt("Ga je naar de kind zijn moeder toe om sorry te zeggen?");
+	}
+
+	if(plan3 == "ja"){
+	alert("Je loop naar de moeder toe, en zegt sorry");
+	alert("ZE WORD GEK EN BELT DE POLITIE");
+	var plan4 = prompt("Vermoord je haar om te voorkomen dat ze de politie belt?");
+	}
+
+	if(plan4 == "ja"){
+	alert("Je steekt haar, en rent weg");
+	alert("Je gaat naar de bieder, en pakt de 5miljoen euro");
+	alert("Je verhuist naar het buitenland, en word nooit gevonden");
+	alert("U win");
+	}
+	
+	else if (keuzes == "2"){
+	alert("Oke je hebt er dus voor gekozen om je beste vriends moeder te vermoorden, focktop");
+	var keuzes1 = prompt("Wil je haar stiekem vermoorden of openbaar?")
+	}
+
+	if(keuzes1 == "stiekem"){
+	alert("Oke je wilt haar dus stiekem vermoorden");
+	var keuzes2 = prompt("Met een sniper of met een auto?");
+	}
+
+ 	if(keuzes2 == "sniper"){
+	alert("Je kiest voor de sniper");
+	alert("Je zoekt een goed plekje op in de weekend om haar te snipen");
+	var keuzes3 = prompt("Bij de supermarkt of voor haar deur?");
+	}
+
+	if(keuzes3 == "supermarkt"){
+	alert("Je pakt de sniper");
+	alert("Je ziet haar");
+	alert("Je staat klaar om te schieten");
+	var keuzes4 = prompt("normaal schieten of trickshotten voor een clip?");
+	}
+
+	if(keuzes4 == "normaal"){
+	alert("Je schiet");
+	alert("Ze is dood");
+	alert("Je ontsnapt, pakt de miljoen euro, en ben nooit meer te zien");
+	}
+
+
+
+
+
+
+
+
+
+
+	
+	
+	
+
+	
+	
